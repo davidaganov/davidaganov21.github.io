218f40e5a5b135d610e3645674b4759dc9c79958
David <davidaganov21@gmail.com>
2022-10-06 17:59:31 +0500

    :hammer_and_wrench: Update block

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/general/Header"
 import About from "./components/blocks/About"
-import Skills from "./components/blocks/Skills"
-import Work from "./components/blocks/Work"
+// import Skills from "./components/blocks/Skills"
+// import Work from "./components/blocks/Work"
 import Footer from "./components/general/Footer"
 
 export default function App() {
@@ -10,8 +10,8 @@ export default function App() {
       <Header />
       <main>
         <About />
-        <Skills />
-        <Work />
+        {/* <Skills />
+        <Work /> */}
       </main>
       <Footer />
     </div>
--- src/components/blocks/About.tsx
+++ src/components/blocks/About.tsx
@@ -10,6 +10,51 @@ export default function About() {
             <span>01.</span> About Me
           </a>
         </h2>
+
+        <div className="about__content">
+          <div className="about__left">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis tenetur libero
+              deserunt, animi quas veritatis facilis illo numquam sed itaque accusantium rem dolore
+              harum magnam{" "}
+              <a
+                href="#"
+                className="inline-link"
+              >
+                ducimus nulla
+              </a>{" "}
+              maxime voluptas! Praesentium.
+            </p>
+            <p>
+              Ipsum,{" "}
+              <a
+                href="#"
+                className="inline-link"
+              >
+                exercitationem
+              </a>{" "}
+              nobis, mollitia aliquid harum saepe veritatis eius modi, consectetur vel quisquam
+              voluptatem perferendis tempora?{" "}
+              <a
+                href="#"
+                className="inline-link"
+              >
+                Optio
+              </a>{" "}
+              voluptatum explicabo pariatur veniam ducimus.
+            </p>
+            {/* Привет! Меня зовут Бриттани, и мне нравится создавать вещи, которые живут в Интернете. Мой интерес к веб-разработке начался еще в 2012 году, когда я решил попробовать редактировать собственные темы Tumblr — оказалось, что создание пользовательской кнопки для реблога научило меня многому в HTML и CSS!
+
+              Перенесемся на сегодняшний день, и я имел честь работать в рекламное агентство, стартап, огромная корпорация, а также студенческая дизайн-студия. В настоящее время мое основное внимание сосредоточено на создании доступных, инклюзивных продуктов и цифрового опыта вУточнениедля самых разных клиентов.
+
+              я тоже недавно запустил курс который охватывает все, что вам нужно для создания веб-приложения с API Spotify с использованием Node & React.
+
+              Вот несколько технологий, с которыми я недавно работал: */}
+          </div>
+          <div className="about__right">
+            <div className="about__picture"></div>
+          </div>
+        </div>
       </div>
     </section>
   )
