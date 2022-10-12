6416d85c5296804caa06d5c34164e7c1a8558730
David <davidaganov21@gmail.com>
2022-10-12 16:14:22 +0500

    :art: Update style

--- src/components/blocks/About.tsx
+++ src/components/blocks/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section
-      className="about"
+      className="about section-ltr"
       id="about"
     >
       <div className="about__inner inner">
