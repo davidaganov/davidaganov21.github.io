ed3b843aadc60755805bb2dc2d71a251288b8d3b
David <davidaganov21@gmail.com>
2022-12-14 12:02:32 +0500

    :wrench: Update component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -11,19 +11,29 @@ export const Title = ({
   ...props
 }: TitleProps): JSX.Element => {
   return (
-    <h2
+    <div
       className={cn(styles.title, className, {
         [styles.rtl]: direction === "rtl",
         [styles.ltr]: direction === "ltr"
       })}
       {...props}
     >
+      {direction === "ltr" && (
+        <h2>
+          <span aria-hidden="true">0{number}.</span> {title}
+        </h2>
+      )}
       <a
         href={link}
         aria-label={title}
       >
-        <span>0{number}.</span> {title}
+        #
       </a>
-    </h2>
+      {direction === "rtl" && (
+        <h2>
+          {title} <span aria-hidden="true">0{number}.</span>
+        </h2>
+      )}
+    </div>
   )
 }
