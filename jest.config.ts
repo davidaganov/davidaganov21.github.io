f2d3b80cb31734208ef850b510698a1905cc2b66
David <davidaganov21@gmail.com>
2023-01-20 17:32:18 +0500

    :robot: Update jest settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,5 @@
 module.exports = {
   clearMocks: true,
   setupFiles: ["<rootDir>/src/setupTests.ts"],
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.tsx", "!**/node_modules/**"],
-  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
-  coveragePathIgnorePatterns: ["<rootDir>/src/**/*.props.{ts}", "<rootDir>/src/interfaces/index.ts"]
+  collectCoverage: true
 }
