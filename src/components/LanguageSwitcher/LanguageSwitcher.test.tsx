638bcbde0bad44f9484234ddbb561606dfeb9f93
David <davidaganov21@gmail.com>
2023-01-20 17:32:28 +0500

    :wrench: Added new test

--- src/components/LanguageSwitcher/LanguageSwitcher.test.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import i18n from "../../services/i18n"
 import { LanguageSwitcher } from "./LanguageSwitcher"
 
 describe("Language Switcher component", () => {
@@ -6,6 +8,16 @@ describe("Language Switcher component", () => {
     render(<LanguageSwitcher />)
 
     expect(screen.getAllByRole("option")).toHaveLength(2)
+  })
+
+  it("Language change by click to the button", () => {
+    render(<LanguageSwitcher />)
+
+    expect(i18n.language === "en").toBeTruthy()
+
+    userEvent.click(screen.getByRole("option", { name: /select language russian/i }))
+
+    expect(i18n.language === "ru").toBeTruthy()
   })
 
   it("Language Switcher snapshot", () => {
