5b64e87f9ef04ae9187dacca53f092e20765deb2
David <davidaganov21@gmail.com>
2023-09-14 16:42:00 +0500

    :robot: Update resume

--- src/components/Resume/ui/WorkPost.tsx
+++ src/components/Resume/ui/WorkPost.tsx
@@ -6,6 +6,15 @@ export interface TitleProps {
   children: string
 }
 
+export interface WorkProject {
+  id: number
+  title: string
+  url: string
+  tech: string[]
+  description: string
+  duties: string[]
+}
+
 export interface WorkPostProps {
   data: {
     companyName: string
@@ -15,8 +24,9 @@ export interface WorkPostProps {
     current?: string
     title: string
     location: string
-    description?: string
-    list?: string[]
+    description: string
+    list: string[]
+    projects: WorkProject[]
   }
 }
 
@@ -49,6 +59,7 @@ const styles = StyleSheet.create({
     marginBottom: 15
   },
   description: {
+    marginBottom: 8,
     fontSize: 10,
     fontFamily: "ua-brand",
     color: "#0a192f"
@@ -69,6 +80,43 @@ const styles = StyleSheet.create({
     fontSize: 10,
     fontFamily: "ua-brand",
     color: "#0a192f"
+  },
+  project: {
+    flexDirection: "column",
+    marginBottom: 8
+  },
+  projectLink: {
+    marginTop: 4,
+    textDecoration: "none"
+  },
+  projectTitle: {
+    fontSize: 10,
+    fontFamily: "ua-brand",
+    fontWeight: "bold",
+    color: "#213e73"
+  },
+  projectDescription: {
+    marginTop: 6,
+    marginBottom: 4,
+    fontSize: 10,
+    fontFamily: "ua-brand",
+    color: "#0a192f"
+  },
+  projectDuties: {
+    flexDirection: "row"
+  },
+  projectDutiesItem: {
+    maxWidth: "90%",
+    marginTop: 4,
+    fontSize: 10,
+    fontFamily: "ua-brand",
+    color: "#0a192f"
+  },
+  projectTech: {
+    marginTop: 6,
+    fontSize: 10,
+    fontFamily: "ua-brand",
+    color: "#213e73"
   }
 })
 
@@ -89,24 +137,67 @@ const WorkPlace = ({ children, url }: WorkPlaceProps) => {
 }
 
 export const WorkPost = ({
-  data: { title, companyName, companyUrl, location, startAt, endAt, current, description, list }
+  data: {
+    title,
+    companyName,
+    companyUrl,
+    location,
+    startAt,
+    endAt,
+    current,
+    description,
+    list,
+    projects
+  }
 }: WorkPostProps) => {
   const buildAbout = () => {
-    if (description && description.length > 0) {
-      return <Text style={styles.description}>{description}</Text>
-    } else if (list && list.length > 0) {
-      return list.map((item, index) => (
-        <View
-          key={index}
-          style={styles.list}
-        >
-          <Text style={styles.mark}>•</Text>
-          <Text style={styles.item}>{item}</Text>
-        </View>
-      ))
-    } else {
-      return null
-    }
+    return (
+      <View>
+        {description && description.length > 0 && (
+          <Text style={styles.description}>{description}</Text>
+        )}
+        {list &&
+          list.length > 0 &&
+          list.map((item, index) => (
+            <View
+              key={index}
+              style={styles.list}
+            >
+              <Text style={styles.mark}>•</Text>
+              <Text style={styles.item}>{item}</Text>
+            </View>
+          ))}
+        {projects &&
+          projects.length > 0 &&
+          projects.map((item, index) => (
+            <View
+              key={index}
+              style={styles.project}
+            >
+              <Link
+                src={item.url}
+                style={styles.projectLink}
+              >
+                <Text style={styles.projectTitle}>{item.title}</Text>
+              </Link>
+
+              <Text style={styles.projectDescription}>{item.description}</Text>
+
+              {item.duties.map((item, index) => (
+                <View
+                  key={index}
+                  style={styles.projectDuties}
+                >
+                  <Text style={styles.mark}>•</Text>
+                  <Text style={styles.projectDutiesItem}>{item}</Text>
+                </View>
+              ))}
+
+              <Text style={styles.projectTech}>{item.tech.join(", ")}</Text>
+            </View>
+          ))}
+      </View>
+    )
   }
 
   const about = buildAbout()
